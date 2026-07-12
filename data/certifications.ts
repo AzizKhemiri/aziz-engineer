3e2206197a4f112c63dc3ba9571cf1e6678f7595
Aziz Khemiri <azizkhemiri109@gmail.com>
2026-07-12 21:33:13 +0100

    Add cert Cyber Defense Analyst

--- data/certifications.ts
+++ data/certifications.ts
@@ -37,7 +37,7 @@ export const certifications: Certification[] = [
     link: "https://www.freecodecamp.org/certification/AzizKhemiri/javascript-algorithms-and-data-structures",
   },
   {
-    name: "Cybersecurity Defense Analyst Career Path",
+    name: "Cyber Security Defense Analyst Career Path",
     issuer: "Cisco Networking Academy - Splunk",
     period: null,
     link: "https://www.credly.com/badges/4753a0c6-b246-45f5-94d2-fcc8cdaab383",
